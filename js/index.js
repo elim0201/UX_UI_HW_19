565fc4a5cb31cbbc7d36b4a8b456893c56ff6e76
estherlim <77757850+elim0201@users.noreply.github.com>
2021-06-14 15:36:41 -0700

    Add hover state to cards (jquery)

--- js/index.js
+++ js/index.js
@@ -1,1 +1,37 @@
 console.log("Your index.js files is loaded correctly!");
+
+$(document).ready(function(){
+
+    $('.flexDiv').hover(
+        // trigger when mouse hover
+        function(){
+            $(this).animate({
+                marginTop: "-=5%",
+            },200);
+        },
+        //trugger when mouse out
+        function(){
+            $(this).animate({
+                marginTop: "0%"
+            },200);
+        }
+    );
+});
+
+$(document).ready(function(){
+
+    $('.project').hover(
+        // trigger when mouse hover
+        function(){
+            $(this).animate({
+                marginTop: "-=5%",
+            },200);
+        },
+        //trugger when mouse out
+        function(){
+            $(this).animate({
+                marginTop: "0%"
+            },200);
+        }
+    );
+});
